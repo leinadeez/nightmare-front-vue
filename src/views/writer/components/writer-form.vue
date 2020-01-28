08d74772429e2008c7f996d0e5b59e53ce88dc19
leinadeez <leinadeez@gmail.com>
2020-01-28 16:20:04 -0300

    Adds some beauty (but not much)

--- src/views/writer/components/writer-form.vue
+++ src/views/writer/components/writer-form.vue
@@ -1,11 +1,22 @@
 <template>
-    <form v-on:submit="submitPost" id="formPost">
-        <input type="text" class="postTitle" v-model="postTitle" name="postTitle" placeholder="Title" />
-        <input type="text" class="postSubtitle" v-model="postSubtitle" name="postSubtitle" placeholder="Subtitle" />
-        <textarea placeholder="Write your text here" v-model="postContent" name="postContent" />
-        <input type="text" class="postTags" name="postTags" v-model="postTags" placeholder="Type your tags" />
-        <button type="submit">Publish!</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <h1>Write your content</h1>
+        </div>
+        <div class="row">
+            <form v-on:submit="submitPost" id="formPost" class="col-8s">
+                <label>Title
+                <input type="text" class="postTitle" v-model="postTitle" name="postTitle" placeholder="Title" /></label>
+                <label>Subtitle
+                <input type="text" class="postSubtitle" v-model="postSubtitle" name="postSubtitle" placeholder="Subtitle" /></label>
+                <label>Content
+                <textarea placeholder="Write your text here" v-model="postContent" name="postContent" /></label>
+                <!--<label>Tags
+                <input type="text" class="postTags" name="postTags" v-model="postTags" placeholder="Type your tags" /></label>-->
+                <button type="submit">Publish!</button>
+            </form>
+        </div>
+    </div>
 </template>
 <script>
 import axios from 'axios'
@@ -24,7 +35,7 @@ export default {
     methods: {
         submitPost(event) {
             event.preventDefault()
-            axios.post('/api/posts', {
+            axios.post('http://192.168.0.16:3000/posts', {
                 "title": this.postTitle,
                 "subtitle": this.postSubtitle,
                 "content": this.postContent,
@@ -43,5 +54,18 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-
+label {
+    width: 100%;
+}
+input[type='text'], textarea{
+    width: 100%;
+    background-color: #eee;
+    border: 1px solid $border-color;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    opacity: 1;
+}
+textarea {
+    height: 300px;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,23 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://192.168.0.16:3000/posts/',
+                target: 'http://192.168.0.16:3000/',
                 changeOrigin: true,
                 pathRewrite: {
-                    '^/api': ''
+                    '^/api': '/'
                 }
             }
         }
+    },
+    css: {
+        loaderOptions: {
+            scss: {
+                prependData: `
+                    @import "~@/assets/scss/_variables.scss";
+                    @import "~@/assets/scss/_mixins.scss";`
+            }
+        }
     }
+
 }
 
